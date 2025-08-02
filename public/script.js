687042f48ffb210c4e85539a07d97e3e5887c8d0
Taylor Stinchcomb <tstinchcomb@proton.me>
2025-08-01 16:49:00 -0800

    initialized hugo project

---
+++ public/script.js
@@ -0,0 +1,23 @@
+const surveyJson = {
+    elements: [{
+        name: "FirstName",
+        title: "Enter your first name:",
+        type: "text"
+    }, {
+        name: "LastName",
+        title: "Enter your last name:",
+        type: "text"
+    }]
+};
+
+const survey = new Survey.Model(surveyJson);
+function alertResults (sender) {
+    const results = JSON.stringify(sender.data);
+    alert(results);
+}
+
+survey.onComplete.add(alertResults);
+
+document.addEventListener("DOMContentLoaded", function() {
+    survey.render(document.getElementById("surveyContainer"));
+});
