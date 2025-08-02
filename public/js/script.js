1ec5986ea0b8e27996f978cd9bc06a35d90ba29e
Taylor Stinchcomb <tstinchcomb@proton.me>
2025-08-01 17:28:20 -0800

    Built first image picker question

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,47 @@
 const surveyJson = {
-    elements: [{
+    elements: [
+        {
         name: "FirstName",
         title: "Enter your first name:",
         type: "text"
-    }, {
-        name: "LastName",
-        title: "Enter your last name:",
-        type: "text"
-    }]
+        }, 
+        {
+            name: "LastName",
+            title: "Enter your last name:",
+            type: "text"
+        },
+        {
+        "type": "imagepicker",
+        "name": "pollobs",
+        "title": "Which of these pollinators have you seen?",
+        "description": "Please select all that apply.",
+        "isRequired": true,
+        "choices": [
+          {
+            "value": "cryptic",
+            "imageLink": "images/pollinators/cryptic.jpg",
+            "text": "Bumblebee (white tailed)"
+          },
+          {
+            "value": "swallowtail",
+            "imageLink": "images/pollinators/swallowtail.jpeg",
+            "text": "Swallowtail Butterfly"
+          },
+          {
+            "value": "hummermoth",
+            "imageLink": "images/pollinators/hummer.jpeg",
+            "text": "Hummingbird Moth"
+          },
+          {
+            "value": "syrphid",
+            "imageLink": "images/pollinators/syrphid.jpeg",
+            "text": "Syrphid Fly"
+          }
+        ],
+        "showLabel": true,
+        "multiSelect": true
+        }
+    ]
 };
 
 const survey = new Survey.Model(surveyJson);
