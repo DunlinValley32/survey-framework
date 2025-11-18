3046915adc752d20a43a4a92fa8b8913a88a9b56
Taylor Stinchcomb <tstinchcomb@proton.me>
2025-11-17 21:06:00 -0900

    typo fix

--- static/js/script.js
+++ static/js/script.js
@@ -43,7 +43,7 @@ const surveyJson = {
         "type": "matrix",
         "name": "familiarity",
         "title": "On a scale of 1-5, indicate your level of familiarity with the following groups of pollinating insects in Alaska:",
-        "columns=": [
+        "columns": [
           { 
             "value": "1", 
             "text": "Not familiar at all" 
